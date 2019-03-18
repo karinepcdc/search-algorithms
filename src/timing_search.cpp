9664c024d7586f6837fed50b1cb52e2ff4104bd1
Karine Piacentini Coelho da Costa <karinepcdc@A202-14L.imd.local>
2019-03-18 18:27:48 -0300

    starting to construct timing_search function

---
+++ src/timing_search.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <chrono>
+
+#include "../includes/searching.h"
+
+/* Test maximum vector size */
+/* 
+   void maxVecTest() {
+    std::vector<char> v;
+    std::cout << "Tamanho máximo: " << v.max_size() << std::endl;
+}
+*/
+
+void fillVec(){
+  std::vector<int> V; // test array
+  
+  V.resize(V.max_size());
+  
+  
+}
+
+int main(int argc, char *argv[]){
+
+
+
+  return 0;
+}
