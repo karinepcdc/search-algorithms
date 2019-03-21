8435a2f056f09b9ba2ffe6890140ec367551152f
Karine Piacentini Coelho da Costa <karinepcdc@A202-08L.imd.local>
2019-03-21 17:42:24 -0300

    Fibonacci search algorithm

--- src/fibonacci_search.cpp
+++ src/fibonacci_search.cpp
@@ -11,6 +11,24 @@ using value_type = long int ; //!< Simple alias to help code maintenance.
  * \param value The target value we are looking for within the range 
  * \return A pointer to the target value withing the range; or last, in case the value is not in the range. 
  */
+
+// return 
+long int *Fib(int n){
+
+  long int *Fib_n = new Fib[n];
+
+  Fib[0]=1;
+  Fib[1]=1;
+
+  for(i=2; i<n; i++){
+    Fib[i] = Fib[n-1] + Fib[n-2];
+  }
+
+  return Fib;
+  
+}
+
+
 const value_type * fibsearch(const value_type *first, const value_type *last, value_type value){
 
 
